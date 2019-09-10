309616cc7abf2d17a8817959f263d0fee3f8660f
Gbubemi Smith <gbubemismith@Gbubemis-MacBook-Pro.local>
2019-09-10 23:02:32 +0100

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+console.log('Client js loaded'); 
+
+
+
+
+const weatherForm = document.querySelector('form');
+
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+
+ 
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    messageOne.textContent = 'Loading ...';
+
+    const location  = search.value;
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            messageOne.textContent = data.error;
+            messageTwo.textContent = '';
+        }
+        else {
+            messageOne.textContent =  data.location;
+            messageTwo.textContent = data.forecast;
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+        
+    });
+});
+
+
+    console.log(location);
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.darksky.net/forecast/2c14fee3c8958c546927dac83d673cb0/' + lat + ',' + long + '?units=si&lang=en';
+
+    request({url: url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined);
+        }else if (body.error){
+            callback('Unable to find weather forecast!', undefined);
+        }else {
+            callback(undefined, body.daily.data[0].summary +' It is currently ' + body.currently.temperature + ' degrees out. There is a ' +
+                    body.currently.precipProbability + '% chance of rain.');
+        }
+    });
+
+
+};
+
+
+
+
+
+module.exports = forecast;
